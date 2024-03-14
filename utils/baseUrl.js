6cbd0cfa191af7535b91ab6585729273d783e002
wijard <brandongalli32@gmail.com>
2024-03-14 10:08:55 -0400

    update production url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://reton-react.hibootstrap.com' 
+? 'https://brandongalli.com' 
 : 'http://localhost:3000';
 
 export default baseUrl;
